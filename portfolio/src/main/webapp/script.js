dad54a0112ee34ea0769caad4d0d71f8859afc8c
ElmerAdrianV <elmeradrianv@outlook.com>
2021-06-14 01:39:28 +0000

    Practice function Fetch

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,12 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+async function addGreetingElmer(){
+
+    const responseFromServer = await fetch('/hello');
+    const textFromResponse = await responseFromServer.text();
+
+    const dateContainer = document.getElementById('greeting-container');
+    dateContainer.innerText = textFromResponse;
+    
+}
