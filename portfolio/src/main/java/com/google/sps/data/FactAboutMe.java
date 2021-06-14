508dddc034a030220511db38371c1a8076e91c18
ElmerAdrianV <elmeradrianv@outlook.com>
2021-06-14 15:10:26 +0000

    add a altAttribute a Fact class

--- portfolio/src/main/java/com/google/sps/data/FactAboutMe.java
+++ portfolio/src/main/java/com/google/sps/data/FactAboutMe.java
@@ -3,15 +3,21 @@ package com.google.sps.data;
 
 public final class FactAboutMe{
     private final String pathImage;
+    private final String altAttribute;
     private final String fact;
 
-    public FactAboutMe(String pathImage, String fact){
+    public FactAboutMe(String pathImage, String altAttribute,String fact){
         this.pathImage = pathImage;
+        this.altAttribute = altAttribute;
         this.fact = fact;
     }
 
     public String getPathImage(){
         return pathImage;
+    }
+
+    public String getAltAttribute(){
+        return altAttribute;
     }
 
     public String getFact(){
