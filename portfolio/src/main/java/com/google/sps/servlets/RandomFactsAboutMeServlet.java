32f4c918b07c0561a06869dcd74faecdc8ea371f
ElmerAdrianV <elmeradrianv@outlook.com>
2021-06-14 15:11:42 +0000

    create a convertJson function and save som facts in EN and ES

--- portfolio/src/main/java/com/google/sps/servlets/RandomFactsAboutMeServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/RandomFactsAboutMeServlet.java
@@ -16,31 +16,82 @@ public class RandomFactsAboutMeServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Create a ArrayList that will contains FactsAboutMe
-    ArrayList<FactAboutMe> factsAboutMe = new ArrayList<>();
+    ArrayList<FactAboutMe> factsAboutMeEN = new ArrayList<>();
+    ArrayList<FactAboutMe> factsAboutMeES = new ArrayList<>();
     
-    // Add FactsAboutMe;
-    factsAboutMe.add( new FactAboutMe("images/logof1.png", "I'm a F1 fans since 2020"));
+    // Add FactsAboutMeEn in English;
+    factsAboutMeEN.add( new FactAboutMe("images/logof1.png", "F1 logo","I'm a F1 fan since 2020"));
+    factsAboutMeEN.add( new FactAboutMe("images/18number.png", "18 Number","I'm 18 years old"));
+    factsAboutMeEN.add( new FactAboutMe("images/birthdayCake.png", "Birthday cake","My birthday is August 7th"));
+    factsAboutMeEN.add( new FactAboutMe("images/ninaGP.jpeg", "Guinea pig","I have guinea pig of pet called Nina"));
+    factsAboutMeEN.add( new FactAboutMe("images/footballer.png", "footballer", "My frustrated dream is to be a footballer (soccer)"));
+    factsAboutMeEN.add( new FactAboutMe("images/elmer-elephant.jpg", "Elmer elephant", "An elephant, famous latin singer, Looney Toon and me are call Elmer"));
+    factsAboutMeEN.add( new FactAboutMe("images/brokenBone.png", "Broken bone", "I have never broken a bone"));
+    factsAboutMeEN.add( new FactAboutMe("images/teacher.png", "Teacher Icon","As a child he hates teaching, now I love being a teacher."));
     
+    // Add FactsAboutMeES in spanish;
+    factsAboutMeES.add( new FactAboutMe("images/logof1.png", "F1 logo","Desde 2020, soy fan de la Formula 1"));
+    factsAboutMeES.add( new FactAboutMe("images/18number.png", "18 Number","Tengo 18 años de edad"));
+    factsAboutMeES.add( new FactAboutMe("images/birthdayCake.png", "Birthday cake","Mi cumpleaños es el 7 de Agosto"));
+    factsAboutMeES.add( new FactAboutMe("images/ninaGP.png", "Guinea pig","Mi mascota es un cuyo llamdo Nina"));
+    factsAboutMeES.add( new FactAboutMe("images/footballer.png", "footballer", "Mi sueño frustado es ser futbolista"));
+    factsAboutMeES.add( new FactAboutMe("images/elmer-elephant.png", "Elmer elephant", "Un elefante, un famoso cantante latino, una caricatura de Looney Toons y yo nos llamamos Elmer"));
+    factsAboutMeES.add( new FactAboutMe("images/brokenBone.png", "Broken bone", "Nunca me he roto un hueso"));
+    factsAboutMeES.add( new FactAboutMe("images/teacher.png", "Teacher Icon","De niño nunca quise enseñar. Ahora, adoro dar clases."));
     //Select randomly a fact in the arrayList
-    int index = Math.round( (float) Math.random()*factsAboutMe.size() );
-    FactAboutMe fact = factsAboutMe.get(index);
-    String json = convertToJson( fact );
+    String json = convertToJson( factsAboutMeEN, factsAboutMeES );
 
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
 
-  private String convertToJson(FactAboutMe fact) {
+  private String convertToJson(ArrayList<FactAboutMe> factsAboutMeEN, ArrayList<FactAboutMe> factsAboutMeES ) {
     StringBuilder json = new StringBuilder();
     
     json.append("{");
-    json.append("\"pathImage\": ");
-    json.append("\"" + fact.getPathImage() + "\"");
-    json.append(",");
-    json.append("\"fact\": ");
-    json.append("\"" + fact.getFact() + "\"");
-    json.append("}");
+    
+    json.append("\"length\" : "+factsAboutMeEN.size()+",");
 
+    json.append("\"factsInEN\" : ");
+    json.append("[");
+    for(FactAboutMe fact : factsAboutMeEN){
+        json.append("{");
+        json.append("\"pathImage\" : ");
+        json.append("\"" + fact.getPathImage() + "\"");
+        json.append(",");
+        json.append("\"altAttribute\" : ");
+        json.append("\"" + fact.getAltAttribute() + "\"");
+        json.append(",");
+        json.append("\"factText\" : ");
+        json.append("\"" + fact.getFact() + "\"");
+        json.append("},");
+    }
+    
+    //Delete the last unnecessary comma
+    json.deleteCharAt(json.length()-1);
+
+    json.append("],");
+    
+    json.append("\"factsInES\" : ");
+    json.append("[");
+    for(FactAboutMe fact : factsAboutMeES){
+        json.append("{");
+        json.append("\"pathImage\" : ");
+        json.append("\"" + fact.getPathImage() + "\"");
+        json.append(",");
+        json.append("\"altAttribute\": ");
+        json.append("\"" + fact.getAltAttribute() + "\"");
+        json.append(",");
+        json.append("\"factText\": ");
+        json.append("\"" + fact.getFact() + "\"");
+        json.append("},");
+    }
+    //Delete the last unnecessary comma
+    json.deleteCharAt(json.length()-1);
+    
+    json.append("]");
+    json.append("}");
+    
     return json.toString();
   }
 }
